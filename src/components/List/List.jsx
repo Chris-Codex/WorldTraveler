05960b21163387598a5c30d48eab24e210df8daf
Christian Obinna Iheacho <christianobinna.iheacho@student.griffith.ie>
2022-06-05 01:45:56 +0100

    Populate list of images based on location

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -6,22 +6,10 @@ import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 import useStyles from './styles';
 
-const List = () => {
+const List = ({ places }) => {
   const classes = useStyles();
   const [type, setType] = useState('restaurants')
   const [rating, setRating] = useState('')
-
-  const places = [
-    { name: 'Cool Place' },
-    { name: 'Cool Place' },
-    { name: 'Cool Place' },
-    { name: 'Cool Place' },
-    { name: 'Cool Place' },
-    { name: 'Cool Place' },
-    { name: 'Cool Place' },
-    { name: 'Cool Place' },
-  ]
-
 
   return (
    <div className={classes.container}>
